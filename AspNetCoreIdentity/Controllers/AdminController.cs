86ffe3895a72ea7afa7cd0ccaff64bbe51e4eda8
Mustafa Ozkan <mstfa.ozkan6655@gmail.com>
2020-03-01 17:58:49 +0300

    Add project files.

---
+++ AspNetCoreIdentity/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreIdentity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreIdentity.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<AppUser> userManager { get; }
+        public AdminController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View(userManager.Users.ToList());
+        }
+    }
+}
